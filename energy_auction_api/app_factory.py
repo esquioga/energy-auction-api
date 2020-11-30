17a9d43987ceeb07b9dd4c22acd364436ebe97c1
João Esquioga <joao.esquioga@thera.com.br>
2020-11-30 11:51:18 -0300

    added cors validation

--- energy_auction_api/app_factory.py
+++ energy_auction_api/app_factory.py
@@ -3,19 +3,18 @@ import logging
 
 from fastapi import FastAPI
 from pymongo import MongoClient
+from fastapi.middleware.cors import CORSMiddleware
 
 from .controllers.auction import AuctionController
 from .utils.controller_registry import register_controller
 from .routes.auction import ROUTER as auction_router
 
 LOGGER = logging.getLogger(__name__)
-LOGGER.info('foi')
 
 
 def create_app(conf):
     """App factory creation function"""
     app = FastAPI()
-    LOGGER.info('create')
     mongodb_client = MongoClient(conf.get('MONGODB_URL'))
     database = mongodb_client[conf['MONGODB_DB']]
     auction_collection = database[conf['MONGODB_COLLECTION']]
@@ -25,5 +24,13 @@ def create_app(conf):
     register_controller(app, auction_controller)
 
     app.include_router(auction_router)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[conf.get('UI_URL', '*')],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     return app
